4ff8c8d9d08f4712e1d3b44a266bfcf960dd8815
Wenew Zhang <wenewboy@gmail.com>
2026-06-10 11:29:15 +0800

    links for docs

--- upload.sh
+++ upload.sh
@@ -5,12 +5,14 @@ REMOTE_HOST="154.17.21.121"
 REMOTE_DIR="/home/onenas/"
 
 echo "正在同步文件到 ${REMOTE_HOST}:${REMOTE_DIR} ..."
-echo "排除规则: *.py, *.sh"
+echo "排除规则: *.py, *.sh, docs/mkdocs.yml, docs/docs"
 
 rsync -avz \
   --exclude='.git' \
   --exclude='*.py' \
   --exclude='*.sh' \
+  --exclude='docs/mkdocs.yml' \
+  --exclude='docs/docs' \
   ./ \
   "${REMOTE_HOST}:${REMOTE_DIR}"
 
